c76abf46c1297112601f2c676ce0aa3a4e05e357
abiola-arobieke <abiola.arobieke@gmail.com>
2023-09-20 14:36:56 +0100

    Add handleDelete method to ReservationList.js file

--- src/components/reservation/ReservationList.js
+++ src/components/reservation/ReservationList.js
@@ -8,7 +8,10 @@ import {
   Scrollbar,
   A11y,
 } from 'swiper/modules';
-import { getReservation } from '../../redux/reservation/reservationSlice';
+import {
+  getReservation,
+  delReservation,
+} from '../../redux/reservation/reservationSlice';
 import ReservationItem from './ReservationItem';
 
 const ReservationList = () => {
@@ -16,10 +19,17 @@ const ReservationList = () => {
   const reservation = useSelector(
     (state) => state.reservations.reservationList,
   );
+  const apiUrl = 'http://127.0.0.1:3000/reservations';
 
   useEffect(() => {
     dispatch(getReservation());
   }, [dispatch]);
+
+  const handleDelete = (id) => {
+    const url = `${apiUrl}/${id}`;
+    dispatch(delReservation(url));
+    console.log(id);
+  };
 
   return (
     <div className="main">
@@ -32,7 +42,11 @@ const ReservationList = () => {
       >
         <>
           {reservation.map((reservation) => (
-            <ReservationItem key={reservation.id} reservation={reservation} />
+            <ReservationItem
+              key={reservation.id}
+              reservation={reservation}
+              handleDelete={handleDelete}
+            />
           ))}
         </>
       </Swiper>
